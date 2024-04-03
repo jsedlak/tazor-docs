f04bed99e9ab41757504adb9acd913773ca130b0
John Sedlak <john.sedlak@outlook.com>
2024-04-02 22:43:56 -0400

    Updates theming as per tazor, updates getting started, adds theme switcher

--- src/Tazor.Docs/Program.cs
+++ src/Tazor.Docs/Program.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tazor.Docs;
-using Tazor.Components;
-using Tazor.Components.Theming;
-using System.Text;
-using System.Diagnostics;
 using Tazor.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Tazor.Docs.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
