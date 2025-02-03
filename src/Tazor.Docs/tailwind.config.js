9b5eb9adb985cb50393b151a9e6900356ec99a75
John Sedlak <john.sedlak@outlook.com>
2025-02-02 21:55:19 -0500

    starts prep for tw4

--- src/Tazor.Docs/tailwind.config.js
+++ src/Tazor.Docs/tailwind.config.js
@@ -1,12 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-//let fs = require("fs")
-
-//const readFileLines = filename =>
-//    fs.readFileSync(filename)
-//        .toString('UTF8')
-//        .split('\n');
-
-//let knownClasses = readFileLines('tailwind.classes.txt');
 
 module.exports = {
     darkMode: 'class',
